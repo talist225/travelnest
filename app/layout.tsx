e68fecaf32e5bc4b907d4f11bd72bbeabcdcee48
talist225 <109045845+talist225@users.noreply.github.com>
2023-04-05 12:00:19 +0300

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/Modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/Modals/LoginModal";
+import RentModal from "./components/Modals/RentModal";
+import getCurrentUser from "./actions/getCurrentUser";
+import SearchModal from "./components/Modals/SearchModal";
+
+export const metadata = {
+  title: "Travel Nest",
+  description: "Find your perfect vacation nest",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const currentUser = await getCurrentUser();
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <SearchModal />
+          <RentModal />
+          <LoginModal />
+          <RegisterModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">{children}</div>
+      </body>
+    </html>
+  );
+}
---
+++ app/types/index.ts
@@ -0,0 +1,24 @@
+import { User, Listing, Reservation } from "@prisma/client";
+
+export type SafeListing = Omit<Listing, "createdAt"> & {
+  createdAt: string;
+};
+
+export type SafeReservation = Omit<
+  Reservation,
+  "createdAt" | "startDate" | "endDate" | "listing"
+> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: SafeListing;
+};
+
+export type SafeUser = Omit<
+  User,
+  "createdAt" | "updateAt" | "emailVerified"
+> & {
+  createdAt: string;
+  updateAt: string;
+  emailVerified: string | null;
+};

4b931952aaae105c1458a5e067718647abd7bb94
talist225 <109045845+talist225@users.noreply.github.com>
2023-04-07 14:04:19 +0300

    Add files via upload

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -28,7 +28,7 @@ export default async function getCurrentUser() {
     return {
       ...currentUser,
       createdAt: currentUser.createdAt.toISOString(),
-      updateAt: currentUser.updateAt.toISOString(),
+      updatedAt: currentUser.updateAt.toISOString(),
       emailVerified: currentUser.emailVerified?.toISOString() || null,
     };
   } catch (error: any) {

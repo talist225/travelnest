2c966557d09174d950e6ca9c1edb790b4b9da738
talist225 <109045845+talist225@users.noreply.github.com>
2023-04-08 15:57:59 +0300

    Add files via upload

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -104,7 +104,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
 
   return (
     <Container>
-      <div className="max-w-screen-lg mx-auto">
+      <div className="max-w-screen-lg mx-auto mt-16">
         <div className="flex flex-col gap-6">
           <ListingHead
             title={listing.title}
--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ const Home = async ({ searchParams }: HomeProps) => {
   return (
     <ClientOnly>
       <Container>
-        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 mt-16">
           {listing.map((listing) => {
             return (
               <ListingCard
